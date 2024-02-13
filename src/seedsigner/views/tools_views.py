831537bfdbdba64058d657174a4b3a126a6a6464
3rd Iteration <steve@cryptoguide.tips>
2024-02-13 08:43:56 -0500

    Update tools_views.py

--- src/seedsigner/views/tools_views.py
+++ src/seedsigner/views/tools_views.py
@@ -1185,7 +1185,7 @@ class ToolsMicroSDFlashView(View):
 
         selected_file_num = self.run_screen(
             ButtonListScreen,
-            title="Select Applet",
+            title="Select Image",
             is_button_text_centered=False,
             button_data=microsd_images
         )
@@ -1223,11 +1223,11 @@ class ToolsMicroSDFlashView(View):
 
             self.loading_screen.stop()
 
-            data.stderr = data.stderr.split('\n')
+            data_stderr_split = data.stderr.split('\n')
 
             inNum = 1
             outNum = 0
-            for errorLine in data.stderr:
+            for errorLine in data_stderr_split:
                 if "Records In" in errorLine:
                     inNum = errorLine.split("+")[0]
                     continue
@@ -1317,11 +1317,11 @@ class ToolsMicroSDWipeZeroView(View):
 
         self.loading_screen.stop()
 
-        data.stderr = data.stderr.split('\n')
+        data_stderr_split = data.stderr.split('\n')
 
         inNum = 1
         outNum = 0
-        for errorLine in data.stderr:
+        for errorLine in data_stderr_split:
             if "Records In" in errorLine:
                 inNum = errorLine.split("+")[0]
                 continue
@@ -1373,11 +1373,11 @@ class ToolsMicroSDWipeRandomView(View):
 
         self.loading_screen.stop()
 
-        data.stderr = data.stderr.split('\n')
+        data_stderr_split = data.stderr.split('\n')
 
         inNum = 1
         outNum = 0
-        for errorLine in data.stderr:
+        for errorLine in data_stderr_split:
             if "Records In" in errorLine:
                 inNum = errorLine.split("+")[0]
                 continue
