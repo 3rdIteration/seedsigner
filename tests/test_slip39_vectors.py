1da0a0a0861437947a9b3fb8ca281e47fc8fdf62
3rd Iteration <stevekr@gmail.com>
2025-07-29 22:03:30 -0400

    Fix SLIP39 vector tests

--- tests/test_slip39_vectors.py
+++ tests/test_slip39_vectors.py
@@ -8,7 +8,7 @@ VECTORS_PATH = os.path.join(os.path.dirname(__file__), 'data', 'shamir_vectors.j
 @pytest.mark.parametrize('desc,mnemonics,secret_hex,xprv', json.load(open(VECTORS_PATH)))
 def test_vectors(desc, mnemonics, secret_hex, xprv):
     if secret_hex:
-        combined = shamir_mnemonic.combine_mnemonics(mnemonics)
+        combined = shamir_mnemonic.combine_mnemonics(mnemonics, b"TREZOR")
         assert combined.hex() == secret_hex
     else:
         with pytest.raises(Exception):
