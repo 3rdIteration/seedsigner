a1e260a665eae3357519fd3e4363777958b3f7bc
3rd Iteration <steve@cryptoguide.tips>
2026-01-08 13:27:29 +1000

    Bump version to B7 (#218)

--- src/seedsigner/controller.py
+++ src/seedsigner/controller.py
@@ -121,7 +121,7 @@ class Controller(Singleton):
         rather than at the top in order avoid circular imports.
     """
     
-    VERSION = "0.8.6+Satochip+ERT-B6"
+    VERSION = "0.8.6+Satochip+ERT-B7"
 
     # Declare class member vars with type hints to enable richer IDE support throughout
     # the code.
