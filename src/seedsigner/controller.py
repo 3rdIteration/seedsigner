8e76c813bce3ccc3f1192b580e3c1a332ea2a4a9
3rd Iteration <steve@cryptoguide.tips>
2026-06-12 06:49:02 -0400

    Update version to SeSi-0.8.6+ShSi-B11

--- src/seedsigner/controller.py
+++ src/seedsigner/controller.py
@@ -152,7 +152,7 @@ class Controller(Singleton):
         rather than at the top in order avoid circular imports.
     """
     
-    VERSION = "SeSi-0.8.6+ShSi-B10"
+    VERSION = "SeSi-0.8.6+ShSi-B11"
 
     # Declare class member vars with type hints to enable richer IDE support throughout
     # the code.
